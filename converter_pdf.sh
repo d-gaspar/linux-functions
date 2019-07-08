a2ec7177d6752edbafae30e82ce8c7d0ef0399c3
d-gaspar <52671074+d-gaspar@users.noreply.github.com>
2019-07-08 13:22:21 -0300

    Add files via upload

---
+++ converter_pdf.sh
@@ -0,0 +1,34 @@
+
+path=$1
+output_ext=$2
+
+if [ -z "$output_ext" ]
+then
+	output_ext="png";
+fi
+
+if [ -n $path ]
+then
+	for i in $(ls $path/*pdf); do
+		npages=$(qpdf --show-npages $i)
+
+
+		if [ $npages -eq 1 ]
+		then
+			png_name=$(printf $i | awk 'match($1,/(.*)\.pdf/, a){print substr(a[1], RSTART, RLENGTH-1)}')
+			convert -verbose -density 150 -quality 100 -flatten -sharpen 0x1.0 -trim $i $png_name"."$output_ext 2>/dev/null
+		else			
+			for j in $(seq 0 $(expr $npages - 1)); do
+				png_name=$(printf $i | awk -v npages=$j 'match($1,/(.*)\.pdf/, a){print substr(a[1], RSTART, RLENGTH-1)"_"npages}')
+				convert -verbose -density 150 -quality 100 -flatten -sharpen 0x1.0 -trim $i[$j] $png_name"."$output_ext 2>/dev/null
+				printf $i[$j]" > "$png_name"."$output_ext"\n"
+			done
+		fi
+	done
+else
+	printf "É necessário colocar o caminho"
+fi
+
+# # """convert:not authorized ... """ ERROR
+# sudo vim /etc/ImageMagick-6/policy.xml
+# change '''policy domain="coder" rights="none" pattern="PDF"''' to '''policy domain="coder" rights="read|write" pattern="PDF"'''
